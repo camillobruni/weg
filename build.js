850cc1f9ad21efe5ad18591dfd66d914de6496d4
Camillo Bruni <cbruni@chromium.org>
2026-04-08 17:21:36 +0200

    license

--- build.js
+++ build.js
@@ -1,3 +1,7 @@
+// Copyright (c) 2026, Camillo Bruni.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import * as esbuild from 'esbuild';
 import { readFile, writeFile, mkdir } from 'fs/promises';
 import path from 'path';
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
+// Copyright (c) 2026, Camillo Bruni.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 'use strict';
 
 export function fmtSecs(s: number): string {
